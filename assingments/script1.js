cbfcd6dcb7b731a6ea2157d48b30eebefea3349f
SERoger <musoniroger5@gmail.com>
2024-07-12 11:55:13 -0700

    all html,css,bootstrap,js files

---
+++ assingments/script1.js
@@ -0,0 +1,46 @@
+// //1.
+// function fun1() {
+//     let n1 = 5;
+//     while (n1 <= 450) {
+//         console.log(n1);
+//         n1 = n1 + 5;
+//     }
+// }
+
+// //2.
+// function fun2() {
+//     let n2 = 333;
+//     while (n2 >= 30) {
+//         console.log(n2);
+//         n2 = n2 - 3;
+//     }
+// }
+
+// //3.
+// function fun3() {
+//     let n3 = 50;
+//     while (n3 <= 150) {
+//         if (n3 % 2 === 0) {
+//             console.log(n3);
+//         }
+//         n3 = n3 + 1;
+//     }
+// }
+// //4.
+// function findLeapYears() {
+//     let n4 = 1900;
+//     while (n4 <= 2024) {
+//         if ((n4 % 400 == 0) || (n4 % 4 == 0 && n4 % 100 != 0)) {
+//             console.log(n4)
+//         }
+//         n4++;
+//     }
+// }
+// //execution
+// findLeapYears();
+
+//Hangman Project
+//TicToc Toe
+
+let beat = new Audio('/Recording.m4a');
+beat.play()
